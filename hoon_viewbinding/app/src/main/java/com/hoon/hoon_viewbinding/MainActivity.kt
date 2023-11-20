bb9e4666416979f55a2912752f0ce43aa727e338
boris920308 <jhoonj0308@gmail.com>
2023-11-20 14:53:00 +0900

    [feat] activity viewbinding 적용

--- hoon_viewbinding/app/src/main/java/com/hoon/hoon_viewbinding/MainActivity.kt
+++ hoon_viewbinding/app/src/main/java/com/hoon/hoon_viewbinding/MainActivity.kt
@@ -2,10 +2,19 @@ package com.hoon.hoon_viewbinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hoon.hoon_viewbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvTitle.text = resources.getString(R.string.hello_view_binding)
     }
+
+
 }
